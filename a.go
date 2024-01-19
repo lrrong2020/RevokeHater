568d0688e9ce7e299a5ed999805e605b5bd9d341
Runrong Liu <lrrong2020@gmail.com>
2024-01-19 15:24:15 +0800

    msg debug

--- a.go
+++ a.go
@@ -46,7 +46,7 @@ func main() {
 		userID := sender.ID()
 		fmt.Println("User ID:", userID)
 		if (msg.IsText() && msg.IsSendByGroup()) {
-			fmt.Printf("%+v\n", msg)
+			fmt.Printf("%+v\n", *msg)
 
 			i, err := strconv.ParseInt(strconv.FormatInt(msg.CreateTime, 10), 10, 64)
 			if err != nil {
