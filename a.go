ed70e63ddeb88b8eccb5e6df75c0ff076d189ef9
Runrong Liu <lrrong2020@gmail.com>
2024-01-30 03:12:29 +0800

    space

--- a.go
+++ a.go
@@ -72,11 +72,11 @@ func main() {
 		// fmt.Println("User ID:", userID)
 
 
-		if (msg.IsText() && strings.HasPrefix(msg.Content, "@啊？？？？") ){
-			if (msg.IsText() && strings.HasPrefix(msg.Content, "@啊？？？？不是") ){
+		if (msg.IsText() && strings.HasPrefix(msg.Content, "@啊？？？？ ") ){
+			if (msg.IsText() && strings.HasPrefix(msg.Content, "@啊？？？？ 不是") ){
 				msg.ReplyText("哥们")
 			}
-			if (msg.IsText() && strings.HasPrefix(msg.Content, "@啊？？？？ping") ){
+			if (msg.IsText() && strings.HasPrefix(msg.Content, "@啊？？？？ ping") ){
 				msg.ReplyText("pong")
 			}
 		}
